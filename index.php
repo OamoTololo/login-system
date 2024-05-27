b7d3c12d6555fcea622f67284c1e5d54e00f59aa
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-05-27 19:41:13 +0200

    Created both a bootstrap login and register form page with my own css styling.

--- index.php
+++ index.php
@@ -4,35 +4,69 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Login</title>
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
 
-<header>
-    <nav>
-        <div>
-            <h3>Login System</h3>
-            <ul class="menu-main">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="#">Products</a></li>
-                <li><a href="#">Current Sales</a></li>
-                <li><a href="#">Member+</a></li>
-            </ul>
+<div class="container">
+    <div class="row justify-content-center align-items-center inner-row">
+        <div class="col-md-5">
+            <div class="form-box login-form p-5">
+                <div class="form-title">
+                    <h2>Login</h2>
+                </div>
+                <form action="">
+                    <div class="form-floating mb-3">
+                        <label for="email" class="">Email</label>
+                        <input type="email" class="form-control form-control-sm" placeholder="Email" id="email" autocomplete="off">
+                        <label for="password" class="mt-3">Password</label>
+                        <input type="password" class="form-control form-control-sm" placeholder="Password" id="password">
+                    </div>
+                    <div class="mt-3">
+                        <button type="submit" class="btn btn-success" name="login" id="login">Login</button>
+                    </div>
+                    <div class="mt-3">
+                        <span>Don't have an account?</span> <button href="../register.php" class="p-0 border-0 bg-transparent rounded-pill login-show">Register Here</button>
+                    </div>
+                </form>
+            </div>
+            <div class="form-box register-form p-5">
+                <div class="form-title">
+                    <h2>Register</h2>
+                </div>
+                <form action="">
+                    <div class="form-floating mb-3">
+                        <label for="name" class="">Name</label>
+                        <input type="text" class="form-control form-control-sm" placeholder="Name" id="name" autocomplete="off">
+                        <label for="surname" class="">Surname</label>
+                        <input type="text" class="form-control form-control-sm" placeholder="surname" id="surname" autocomplete="off">
+                        <label for="password" class="mt-3">Password</label>
+                        <input type="password" class="form-control form-control-sm" placeholder="Password" id="password">
+                        <label for="confirmPassword" class="mt-3">Password</label>
+                        <input type="password" class="form-control form-control-sm" placeholder="Confirm Password" id="confirmPassword">
+                        <label for="dob" class="mt-3">Date of Birth</label>
+                        <input type="date" class="form-control form-control-sm" id="dob">
+                    </div>
+                    <div class="mt-3">
+                        <button type="submit" class="btn btn-success" name="register" id="register">Register</button>
+                    </div>
+                    <div class="mt-3">
+                        <span>Already have an account?</span> <button href="index.php" class="p-0 border-0 bg-transparent rounded-pill register-show">Login Here</button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <ul class="menu-member">
-            <li><a href="">Sign Up</a></li>
-            <li><a href="">Login</a></li>
-        </ul>
-    </nav>
-</header>
-
-<section class="index-intro">
-    <div class="index-intro-bg">
-
     </div>
-</section>
+</div>
+
+<script src="js/jquery.js"></script>
+<script src="js/all.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/login.js"></script>
 </body>
 </html>
